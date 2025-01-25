fbd2ee2946decca6d14ea882959115883f156f9d
Md.Ashraful Islam <crackthecode10@gmail.com>
2025-01-25 21:20:33 +0600

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup=async (req,res)=>{
+    res.send('signup route called')
+}
+
+export const login=async (req,res)=>{
+    console.log('login route called')
+}
+
+export const logout=async (req,res)=>{
+    console.log('logout route called')
+}
