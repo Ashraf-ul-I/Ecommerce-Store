27815c21cad7c3876a01cbd64ce3f3371ea2b34b
Md.Ashraful Islam <crackthecode10@gmail.com>
2025-03-22 02:21:12 +0600

    React Router and Setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPages from "./pages/SignUpPages";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+import "../src/style.css";  // Import CSS file
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white relative overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="gradient-bg"></div>
+        </div>
+      </div>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPages />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
